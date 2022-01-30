5e662e0a45f9d6831aa032806f7e531a26fc6a02
ashiqul50021 <ashiqulislamrasel@gmail.com>
2022-01-31 01:49:10 +0600

    variable added

---
+++ variable.js
@@ -0,0 +1,17 @@
+// 16-2 What is variable, five things you need to declare a variable
+// A Variable is like a container that contains various kinds of data for future uses.
+// ● In JavaScript, we have needed five things to declare a variable. They are var (keyword),
+// variableName, = (equal sign), a value, and, a ‘;’ (semicolon at the end).
+// ● Some example: var price = 21; var age = 24; var temp = 37; etc.
+
+var price = 21;
+var age = 24;
+var temp = 37;
+
+var name = "John";
+var address = "dhaka";
+var phone = "0123456789";
+var school = "Dhaka International School";
+
+var pass = true;
+var fail = false;
---
+++ variable.py
@@ -0,0 +1,10 @@
+price = 21
+age = 13
+temp = 21
+
+name = 'Swaroop'
+addres = 'Bangalore'
+phone = '1234567890'
+
+passed = True
+failed = False

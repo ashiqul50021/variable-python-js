4d13fea70d32e751c7ea941b33bf28b29eacd9a7
ashiqul50021 <ashiqulislamrasel@gmail.com>
2022-02-02 01:06:47 +0600

    add python variable

--- variable.py
+++ variable.py
@@ -1,3 +1,45 @@
+#variable are containers for storing data values
+
+#creating a variable
+#python has no command to create a variable
+#A variable is created the moment you assing a value to it
+
+x= 5
+y = "John"
+print(x)
+print(y)
+
+#variable do not need to be declared with perticular type, and can even change type after they have been set.
+x = 4
+x  = "Sally"
+print(x)
+
+# casting
+#casting is the process of changing the type of data stored in a variable
+x = str(3)
+y = int(2.9)
+z = float(2)
+
+#get the type
+#you can get the data type of a variable with the type() function
+x = 5
+y = "john"
+print(type(x))
+print(type(y))
+
+#single or double quotes
+#string variables can be declared either by using single or double quotes
+
+x = " john"
+#is hte same as 
+y = 'john'
+
+#the will create two variables
+a = 4
+A = "sally"
+
+
+
 price = 21
 age = 13
 temp = 21
